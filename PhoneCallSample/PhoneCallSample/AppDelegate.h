58de402b2fe332ee1ed44c36a00b89bbe2192f47
haowen.liang <lianghwen@live.cn>
2014-03-04 19:18:48 -0800

    [Description of Changes] 1，create the initial project [Code review] Reviewer:None
    [Risk Assessment]
    low
    [Sphere of influence]
    
    [Self-test]
    Simulator: OK

---
+++ PhoneCallSample/PhoneCallSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PhoneCallSample
+//
+//  Created by haowenliang on 14-3-5.
+//  Copyright (c) 2014年 dpsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PhoneCallSample/PhoneCallSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PhoneCallSample
+//
+//  Created by haowenliang on 14-3-5.
+//  Copyright (c) 2014年 dpsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
